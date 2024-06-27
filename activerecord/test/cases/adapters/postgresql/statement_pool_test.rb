6703df20440eaa4ee3c381291bcd5874cc79aa18
gendosu <gendosu@gmail.com>
2024-06-27 16:47:00 +0900

    change PGconn to PG:Connection

--- activerecord/test/cases/adapters/postgresql/statement_pool_test.rb
+++ activerecord/test/cases/adapters/postgresql/statement_pool_test.rb
@@ -5,11 +5,11 @@ module ActiveRecord
     class PostgreSQLAdapter < AbstractAdapter
       class InactivePGconn
         def query(*args)
-          raise PGError
+          raise PG::Error
         end
 
         def status
-          PGconn::CONNECTION_BAD
+          PG::Connection::CONNECTION_BAD
         end
       end
 
@@ -31,7 +31,7 @@ module ActiveRecord
         end
 
         def test_dealloc_does_not_raise_on_inactive_connection
-          cache = StatementPool.new InactivePGconn.new, 10
+          cache = StatementPool.new InactivePG::Connection.new, 10
           cache['foo'] = 'bar'
           assert_nothing_raised { cache.clear }
         end
